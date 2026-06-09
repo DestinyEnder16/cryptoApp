bcaa9ed3a4aa16a0ea9dc04f15b15549a632812a
Destiny Israel <destinyprogrammer06@gmail.com>
2026-06-09 06:48:20 +0100

    extracted sections into reusable components

---
+++ src/components/MarketActionsStrip.tsx
@@ -0,0 +1,36 @@
+import { router } from 'expo-router';
+import { StyleSheet, View } from 'react-native';
+import { Colors } from '../constants/styles';
+import MarketActionBtn from './MarketActionBtn';
+
+export default function MarketActionsStrip() {
+  return (
+    <View style={styles.actionBtnsStrip}>
+      <MarketActionBtn
+        text="All"
+        backgroundColor={Colors.green}
+        txtColor={Colors.dark}
+        action={() => console.log('hello')}
+      />
+      <MarketActionBtn
+        text="Gainers"
+        backgroundColor={Colors.secondaryBackgroundColor}
+        txtColor={Colors.text}
+        action={() => router.navigate('/markets/trending')}
+      />
+      <MarketActionBtn
+        text="Watchlist"
+        backgroundColor={Colors.secondaryBackgroundColor}
+        txtColor={Colors.text}
+        action={() => console.log('hello')}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  actionBtnsStrip: {
+    flexDirection: 'row',
+    gap: 10,
+  },
+});
